4f3336f99dba5b1e5d343abfa88336a05777902f
kjsik <127501452+kjsik@users.noreply.github.com>
2025-11-14 22:53:52 +0900

    Add files via upload

--- initial-xv6/xv6-public/sysfile.c
+++ initial-xv6/xv6-public/sysfile.c
@@ -74,11 +74,12 @@ sys_read(void)
   struct file *f;
   int n;
   char *p;
+  
+  readcount++;
 
   if(argfd(0, 0, &f) < 0 || argint(2, &n) < 0 || argptr(1, &p, n) < 0)
     return -1;
 
-  readcount++;
   return fileread(f, p, n);
 }
 
